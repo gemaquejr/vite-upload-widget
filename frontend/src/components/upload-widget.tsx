2e5cc904c77de34c0c048e055db435588dfb8939
Francisco Gemaque <gemaquejr@hotmail.com>
2026-01-23 19:03:34 -0300

    feat: adiciona base de botão e widget colapsável
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- frontend/src/components/upload-widget.tsx
+++ frontend/src/components/upload-widget.tsx
@@ -1,19 +1,30 @@
+import * as Collapsible from "@radix-ui/react-collapsible"
 import { UploadWidgetDropzone } from "./upload-widget-dropzone"
 import { UploadWidgetHeader } from "./upload-widget-header"
 import { UploadWidgetUploadList } from "./upload-widget-upload-list"
+import { useState } from "react";
+import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 
 export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
   return (
-    <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl">
-      <UploadWidgetHeader />
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className="bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl">
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
+        
+        <Collapsible.Content>
+          <UploadWidgetHeader />
 
-      <div className="flex flex-col gap-4 py-3">
-        <UploadWidgetDropzone />
+          <div className="flex flex-col gap-4 py-3">
+            <UploadWidgetDropzone />
 
-        <div className="h-px bg-zinc-800 border-t border-black box-content"></div>
+            <div className="h-px bg-zinc-800 border-t border-black box-content"></div>
 
-        <UploadWidgetUploadList />
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root>
   )
 }
