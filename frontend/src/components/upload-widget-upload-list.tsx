3746a71eeb41ff3098739f31dd9c6ccc35fac654
Francisco Gemaque <gemaquejr@hotmail.com>
2026-01-25 21:39:19 -0300

    feat: adiciona base de componente de upload de itens no componente de upload de lista
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- frontend/src/components/upload-widget-upload-list.tsx
+++ frontend/src/components/upload-widget-upload-list.tsx
@@ -1,5 +1,14 @@
+import { UploadWidgetUploadItem } from './upload-widget-upload-item'  
+
 export function UploadWidgetUploadList() {
   return (
-    <div className="px-3 h-48"></div>
+    <div className="px-3 flex flex-col gap-3">
+      <span className="text-xs font-medium">
+        Uploaded files{' '}
+        <span className="text-zinc-400">(2)</span>
+      </span>
+      <UploadWidgetUploadItem />
+      <UploadWidgetUploadItem />
+    </div>
   )
 }
